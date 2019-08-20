147915627547a61b858bf34d707180bde2172db1
Javier Goldschmidt <javi@elgatoylacaja.com>
2019-08-20 13:41:39 -0300

    Removed purgecss

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,13 +1,5 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-
 module.exports = {
   plugins: {
     autoprefixer: {},
-    purgecss: process.env.NODE_ENV === "production" ? {
-        content: [
-            "./src/**/*.html", 
-            "./src/**/*.vue"
-        ]
-    } : "",
   },
 };
