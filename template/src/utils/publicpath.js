881da936792f56b94e318df9b8a1c40d6bc87532
Javier Goldschmidt <javi@elgatoylacaja.com>
2019-08-21 14:40:05 +0000

    publicpath.js edited online with Bitbucket

--- template/src/utils/publicpath.js
+++ template/src/utils/publicpath.js
@@ -1,3 +1,10 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-undef */
+
+/*
+    Author: Javier Goldschmidt
+    Company: Abre Cultura
+    Date: 10/07/2019 (dd-mm-yyyy)
+    Description: Changes the default hardcoded publicPath for a dynamic one if it exists
+*/
 if (typeof webpackPublicPath !== 'undefined') __webpack_public_path__ = webpackPublicPath;
