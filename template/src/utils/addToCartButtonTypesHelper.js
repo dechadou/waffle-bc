547c6d511e1ea57370f7f841ff6c36e0f49b3d2b
Javier Goldschmidt <javi@elgatoylacaja.com>
2019-08-21 15:10:38 +0000

    addToCartButtonTypesHelper.js edited online with Bitbucket

--- template/src/utils/addToCartButtonTypesHelper.js
+++ template/src/utils/addToCartButtonTypesHelper.js
@@ -1,4 +1,9 @@
 // eslint-disable-next-line import/prefer-default-export
+
+// Expects an OnClick event object
+// Optional: Expects a callback function
+// Attaches a class to the Onclick Event target and detaches it after 1 second
+// Calls to the onFinish callback function after detaching the class
 export const animateButton = (event, onFinish = () => { }) => {
   event.target.classList.add('pulse');
   setTimeout(() => {
