6a2e36b27f2d06ddef7c5ed38f61f625624cd504
Javier Goldschmidt <javi@elgatoylacaja.com>
2019-08-21 13:36:44 +0000

    main.js edited online with Bitbucket

--- template/src/main.js
+++ template/src/main.js
@@ -9,8 +9,13 @@ import App from './App.vue';
 import router from './router';
 // import './registerServiceWorker'; Uncomment for service worker
 
+// Logs waffle version on console
 console.log(`Waffle Release v${getVariable(VariableNames.WaffleVersion)}`);
+
+// Checks if user is trying to load another environment on production and asks for password
 EnvCheck();
+
+// Loads vuex
 const store = getStore();
 
 new Vue({
