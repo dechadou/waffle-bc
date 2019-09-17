4646c0b663049c276ea99cd88e415eec8cff6940
Javier Goldschmidt <javi@elgatoylacaja.com>
2019-09-17 12:53:43 -0300

    Changed 'Nostros' to 'Nosotres'

--- template/src/internal/FooterTypes/Footer-Abre.vue
+++ template/src/internal/FooterTypes/Footer-Abre.vue
@@ -37,60 +37,60 @@
 
 
 <script>
-import { FooterType, Social, Icon } from "@/extendables/BaseComponents";
+import { FooterType, Social, Icon } from '@/extendables/BaseComponents';
 
 // @group FOOTER TYPES
 // Displays the complete ABRE Landing footer
 // @vuese
 export default {
-  name: "Footer-Abre",
+  name: 'Footer-Abre',
   extends: FooterType,
   components: {
     Social,
-    Icon
+    Icon,
   },
   data() {
     return {
       address: {
-        street: "Teodoro García 2474",
-        city: "Ciudad de Buenos Aires",
-        country: "Argentina",
-        show: false
+        street: 'Teodoro García 2474',
+        city: 'Ciudad de Buenos Aires',
+        country: 'Argentina',
+        show: false,
       },
       CTAMail: {
-        copy: "Escribinos a:",
-        email: "hola@abrecultura.com",
-        cta: "mailto:hola@abrecultura.com",
-        show: false
+        copy: 'Escribinos a:',
+        email: 'hola@abrecultura.com',
+        cta: 'mailto:hola@abrecultura.com',
+        show: false,
       },
       menu: [
         {
-          title: "Nosotros",
-          url: "https://abrecultura.com/nosotros/",
+          title: 'Nosotres',
+          url: 'https://abrecultura.com/nosotres/',
           isRoute: false,
-          showForLandscape: true
+          showForLandscape: true,
         },
         {
-          title: "F.A.Q.",
-          url: "https://abrecultura.com/faq/",
+          title: 'F.A.Q.',
+          url: 'https://abrecultura.com/faq/',
           isRoute: false,
-          showForLandscape: true
+          showForLandscape: true,
         },
         {
-          title: "Estado de mi pedido",
-          url: "https://abrecultura.com/mi-pedido/",
+          title: 'Estado de mi pedido',
+          url: 'https://abrecultura.com/mi-pedido/',
           isRoute: false,
-          showForLandscape: true
+          showForLandscape: true,
         },
         {
-          title: "Pasar a buscar",
-          url: "https://abrecultura.com/pasar-a-buscar/",
+          title: 'Pasar a buscar',
+          url: 'https://abrecultura.com/pasar-a-buscar/',
           isRoute: false,
-          showForLandscape: true
-        }
-      ]
+          showForLandscape: true,
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 
--- template/src/internal/HeaderSection.vue
+++ template/src/internal/HeaderSection.vue
@@ -24,7 +24,7 @@
     <nav class="menu" v-if="hasMenu">
       <ul class="nav" v-if="!isProductInternal">
         <li>
-          <a href="https://abrecultura.com/nosotros/">Nosotros</a>
+          <a href="https://abrecultura.com/nosotres/">Nosotres</a>
         </li>
         <li>
           <a href="https://abrecultura.com/nosotros/mi-pedido/">Estado de mi pedido</a>
@@ -36,50 +36,50 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-import { StoreDataNamespace } from "@/store/module/StoreData";
-import { Social, Icon } from "@/extendables/BaseComponents";
+import { mapState } from 'vuex';
+import { StoreDataNamespace } from '@/store/module/StoreData';
+import { Social, Icon } from '@/extendables/BaseComponents';
 
 // @group INTERNAL COMPONENTS
 // The page's header
 // @vuese
 export default {
-  name: "HeaderSection",
+  name: 'HeaderSection',
   props: {
     // Current year. Used for displaying inside the mobile menu
-    year: String
+    year: String,
   },
   components: {
     Social,
-    Icon
+    Icon,
   },
   data() {
     return {
-      isActive: false
+      isActive: false,
     };
   },
   computed: {
-    ...mapState(StoreDataNamespace, ["store_id"]),
+    ...mapState(StoreDataNamespace, ['store_id']),
     currentPage() {
       return this.$route.name;
     },
     isHome() {
-      return this.currentPage === "DefaultHomePage";
+      return this.currentPage === 'DefaultHomePage';
     },
     isProductInternal() {
       return (
-        this.currentPage === "DefaultProductPage" ||
-        this.currentPage === "DefaultBundlePage"
+        this.currentPage === 'DefaultProductPage'
+        || this.currentPage === 'DefaultBundlePage'
       );
     },
     hasMenu() {
       return this.store_id === 8;
-    }
+    },
   },
   watch: {
     $route() {
       this.isActive = false;
-    }
+    },
   },
   methods: {
     /**
@@ -88,8 +88,8 @@ export default {
      */
     toggleMenu() {
       this.isActive = !this.isActive;
-    }
-  }
+    },
+  },
 };
 </script>
 
