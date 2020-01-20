4eca636917a5c581afc1d21d8e42975523fc7f1d
Javier Goldschmidt <javi@elgatoylacaja.com>
2020-01-20 18:53:00 -0300

    Begin currency by location

--- template/src/App.vue
+++ template/src/App.vue
@@ -14,74 +14,78 @@
 </template>
 
 <script>
-import Vue from "vue";
-import VueAnalytics from "vue-analytics";
-import { mapActions, mapGetters, mapState, mapMutations } from "vuex";
-import router from "@/router";
+import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
+import {
+  mapActions, mapGetters, mapState, mapMutations,
+} from 'vuex';
+import router from '@/router';
 import {
   StoreDataActionTypes,
   StoreDataGetterTypes,
   StoreDataMutationTypes,
-  StoreDataNamespace
-} from "@/store/module/StoreData";
-import { ThemeMutationTypes, ThemeNamespace } from "@/store/module/Theme";
-import { BreakpointsMutationTypes } from "@/store/module/Breakpoints";
-import { getVariable, VariableNames } from "@/config";
-import * as FooterTypes from "@/extendables/FooterTypes";
-import { CartHelper } from "@/objects/CartObjects";
+  StoreDataNamespace,
+} from '@/store/module/StoreData';
+import { ThemeMutationTypes, ThemeNamespace } from '@/store/module/Theme';
+import { BreakpointsMutationTypes } from '@/store/module/Breakpoints';
+import { getVariable, VariableNames } from '@/config';
+import * as FooterTypes from '@/extendables/FooterTypes';
+import { CartHelper } from '@/objects/CartObjects';
+import * as Request from '@/utils/request';
 import {
   Loading,
   Cart,
   HeaderSection,
-  RelatedProducts
-} from "@/extendables/BaseComponents";
+  RelatedProducts,
+} from '@/extendables/BaseComponents';
 
 // @group INTERNAL COMPONENTS
 // This is the main component
 export default {
-  name: "App",
+  name: 'App',
   components: {
     ...FooterTypes,
     Loading,
     Cart,
     HeaderSection,
-    RelatedProducts
+    RelatedProducts,
   },
   data() {
     return {
       show: false,
-      cartHelper: null
+      cartHelper: null,
     };
   },
   computed: {
-    ...mapState(ThemeNamespace, ["footer", "themeConfig", "theme"]),
+    ...mapState(ThemeNamespace, ['footer', 'themeConfig', 'theme']),
     ...mapState(StoreDataNamespace, [
-      "data",
-      "template",
-      "error",
-      "year",
-      "store_id",
-      "store_slug"
+      'data',
+      'template',
+      'error',
+      'year',
+      'store_id',
+      'store_slug',
     ]),
     ...mapGetters({
-      isLoaded: StoreDataGetterTypes.IS_LOADED
+      isLoaded: StoreDataGetterTypes.IS_LOADED,
     }),
     hasHeader() {
-      return this.theme === "marketplace";
-    }
+      return this.theme === 'marketplace';
+    },
   },
   watch: {
     isLoaded(value) {
       if (!value) return;
+      this.checkIpForPricing();
       this.setTheme(this.template.template_selector);
       this.setFooter(this.template.footer_type);
       this.setApp();
 
       if (this.$ga) {
-        console.log("page-loaded");
+        console.log('page-loaded');
         this.$ga.event({
           eventCategory: `waffle-${this.store_slug}`,
-          eventAction: "page-loaded"
+          eventAction: 'page-loaded',
         });
       }
     },
@@ -116,7 +120,7 @@ export default {
           scope.show = true;
         }, 500);
       }
-    }
+    },
   },
   created() {
     this.fetchData();
@@ -124,18 +128,27 @@ export default {
     // Tells the Breakpoints Store to get the size of the screen and set the breakpoint
     this.setBreakpoint();
 
-    window.addEventListener("resize", this.onResize, true);
+    window.addEventListener('resize', this.onResize, true);
   },
   methods: {
     ...mapMutations({
       setTheme: ThemeMutationTypes.SET_THEME,
       setFooter: ThemeMutationTypes.SET_FOOTER,
       setStoreIdentifier: StoreDataMutationTypes.SET_STORE_IDENTIFIER,
-      setBreakpoint: BreakpointsMutationTypes.SET_BREAKPOINT
+      setBreakpoint: BreakpointsMutationTypes.SET_BREAKPOINT,
     }),
     ...mapActions({
-      fetchStoreData: StoreDataActionTypes.FETCH_STORE_DATA
+      fetchStoreData: StoreDataActionTypes.FETCH_STORE_DATA,
     }),
+    /**
+     * @vuese
+     * Checks the user's IP and changes pricing if necesary
+     */
+    async checkIpForPricing() {
+      // TODO: Only make the request if tienda internacional
+      const ipinfo = await Request.get('https://ipapi.co/json');
+      console.log(ipinfo);
+    },
     /**
      * @vuese
      * Calls to setBreakpoint on screen resize
@@ -152,7 +165,7 @@ export default {
     fetchData() {
       if (!this.$route.name) return;
       this.fetchStoreData(
-        this.$route.params.slug || getVariable(VariableNames.DefaultSlug)
+        this.$route.params.slug || getVariable(VariableNames.DefaultSlug),
       );
     },
     /**
@@ -170,15 +183,15 @@ export default {
 
       // The store identifier is used by the Cart for saving things on LocalStorage
       this.setStoreIdentifier({
-        domain: "abre",
-        storeSlug: this.store_id
+        domain: 'abre',
+        storeSlug: this.store_id,
       });
 
       // Calls to VueAnalytics component if an Analytics code exists
       if (this.template.codigo_analytics) {
         Vue.use(VueAnalytics, {
           id: this.template.codigo_analytics,
-          router
+          router,
         });
       }
 
@@ -186,7 +199,7 @@ export default {
       // The cartHelper object asks for the storeData
       this.cartHelper = new CartHelper(this.data);
 
-      const loadingScreen = document.getElementById("loadingScreen");
+      const loadingScreen = document.getElementById('loadingScreen');
       loadingScreen.parentElement.removeChild(loadingScreen);
 
       this.show = true;
@@ -197,15 +210,14 @@ export default {
      */
     isValidPage() {
       if (
-        process.env.NODE_ENV === "production" &&
-        this.$route.params.slug &&
-        this.template.tienda_url &&
-        window.location.host.indexOf(this.template.tienda_url) === -1
-      )
-        return false;
+        process.env.NODE_ENV === 'production'
+        && this.$route.params.slug
+        && this.template.tienda_url
+        && window.location.host.indexOf(this.template.tienda_url) === -1
+      ) { return false; }
       return true;
-    }
-  }
+    },
+  },
 };
 </script>
 
