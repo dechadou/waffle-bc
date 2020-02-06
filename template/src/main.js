e8aa72ded89fb6ea59f3bff678b7c1dcf8813006
Javier Goldschmidt <javi@elgatoylacaja.com>
2020-02-06 13:15:12 -0300

    added GTM to Waffle

--- template/src/main.js
+++ template/src/main.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-var */
 import './utils/publicpath';
 import Vue from 'vue';
+import VueGtm from 'vue-gtm';
 import getStore from './store';
 import { EnvCheck } from './utils';
 import { getVariable, VariableNames } from './config';
@@ -17,6 +18,13 @@ EnvCheck();
 
 // Loads vuex
 const store = getStore();
+
+Vue.use(VueGtm, {
+  id: 'GTM-NLPFLCD',
+  debug: true, // Whether or not display console logs debugs (optional)
+  loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+  vueRouter: router, // Pass the router instance to automatically sync with router (optional)
+});
 
 new Vue({
   router,
